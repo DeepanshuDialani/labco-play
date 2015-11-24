e8d36289dcc0763d818b71f5d9418ed4872bf8aa
DeepanshuDialani <deepanshu.dialani@housing.com>
2015-11-25 03:07:03 +0530

    Login & Logout implementation

---
+++ app/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+	
+	public static final String AUTH_TOKEN_KEY = "authToken";
+	
+	public static final String EMAIL_LOGIN_KEY = "email";
+	
+	public static final String PASSWORD_LOGIN_KEY = "password";
+
+}
