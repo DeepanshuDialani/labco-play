ea10d2af3781c55cf2d16093ff30af57025306e6
DeepanshuDialani <deepanshu.dialani@housing.com>
2015-12-09 03:26:33 +0530

    Search box UI clean improvement

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,11 +6,8 @@ import java.util.Map;
 
 import models.DetailRecord;
 import models.LabTest;
-<<<<<<< HEAD
 import models.LabTestParameter;
-=======
 import models.TestFieldData;
->>>>>>> fa16ccf376bcf50d1e37c4b2f644839ab9428f9a
 import play.*;
 import play.mvc.*;
 import play.mvc.Http.RequestBody;
@@ -19,7 +16,7 @@ import utils.Utils;
 import views.html.*;
 
 public class Application extends Controller {
-    
+
 	public Result index() {
 		String user = session(Constants.AUTH_TOKEN_KEY);
 	    if(user != null) {
@@ -94,6 +91,7 @@ public class Application extends Controller {
     	parameters.add(new LabTestParameter(4457, "D2"));
     	parameters.add(new LabTestParameter(4458, "D3"));
     	return parameters;
+    }
 
     public Result detailRecord(Long id) {
     	return ok(detailrecord.render(getDetailRecord()));
@@ -101,24 +99,43 @@ public class Application extends Controller {
     
     private DetailRecord getDetailRecord() {
     	ArrayList<TestFieldData> fields = new ArrayList<>();
-    	fields.add(new TestFieldData("RBC", "4.7 - 6.1", "4.5", false));
-    	fields.add(new TestFieldData("WBC", "4500 - 10000", "5400", true));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("WBC", "4500 - 10000", "5400", true));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	fields.add(new TestFieldData("Hb", "13.5 - 17.5", "9.5", false));
-    	DetailRecord record = new DetailRecord(1, "Akshay", "Male", "Blood", fields);
+    	fields.add(new TestFieldData("RBC", "4.7 - 6.1", "4.8", true));
+    	fields.add(new TestFieldData("WBC", "4500 - 10000", "4300", false));
+    	fields.add(new TestFieldData("Reticulocytes", "13.5 - 17.5", "9.5", false));
+    	fields.add(new TestFieldData("PCV", "13.5 - 17.5", "19.5", false));
+    	fields.add(new TestFieldData("MCV", "13.5 - 17.5", "11", false));
+    	fields.add(new TestFieldData("MCH", "13.5 - 17.5", "14", true));
+    	fields.add(new TestFieldData("Red cell morphology", "0.15-1.5%", "1.6", false));
+    	fields.add(new TestFieldData("Target cells", "13.5 - 17.5", "16", true));
+    	fields.add(new TestFieldData("Ovalocytes", "13.5 - 17.5", "14.3", true));
+    	fields.add(new TestFieldData("Spherocytes", "4500 - 10000", "5400", true));
+    	fields.add(new TestFieldData("Sickle cells", "13.5 - 17.5", "16.7", true));
+    	fields.add(new TestFieldData("Nucleated Red cells", "13.5 - 13.8", "13.5", true));
+    	fields.add(new TestFieldData("Anisocytosis", "13.5 - 17.5", "15.4", true));
+    	fields.add(new TestFieldData("Microcytosis", "13.5 - 17.5", "14.6", true));
+    	fields.add(new TestFieldData("Anisochromasia", "13.5 - 17.5", "19.5", false));
+    	fields.add(new TestFieldData("Punctate Basiophilia", "13.5 - 17.5", "11.5", false));
+    	fields.add(new TestFieldData("Poikilocytosis", "13.5 - 17.5", "16.5", true));
+    	fields.add(new TestFieldData("Hypochromasia", "13.5 - 17.5", "14.4", true));
+    	fields.add(new TestFieldData("Polychromasia", "13.5 - 17.5", "16.2", true));
+    	fields.add(new TestFieldData("Platelet Morphology", "100,000-400,000", "250,000", true));
+    	fields.add(new TestFieldData("WBC", "13.5 - 17.5", "19.5", false));
+    	fields.add(new TestFieldData("White cell", "13.5 - 17.5", "14.2", true));
+    	fields.add(new TestFieldData("Morphology", "13.5 - 17.5", "11.6", false));
+    	fields.add(new TestFieldData("Myeloblasts", "13.5 - 17.5", "12.4", false));
+    	fields.add(new TestFieldData("Promyelocytes", "13.5 - 17.5", "13.5", true));
+    	fields.add(new TestFieldData("Myelocytes", "13.5 - 17.5", "19.5", false));
+    	fields.add(new TestFieldData("Metamyelocyte", "13.5 - 17.5", "9.2", false));
+    	fields.add(new TestFieldData("Polymorphs", "13.5 - 17.5", "17.12", true));
+    	fields.add(new TestFieldData("Eosinophils", "1-3%", "2.3%", true));
+    	fields.add(new TestFieldData("Basophils", "0-0.75%", "0.41%", true));
+    	fields.add(new TestFieldData("Lymphocytes", "25-33%", "11.5%", false));
+    	fields.add(new TestFieldData("Monocytes", "3-7%", "4.6%", true));
+    	fields.add(new TestFieldData("Plasma cells", "13.5 - 17.5", "13.5", true));
+    	fields.add(new TestFieldData("Abnormal cells", "13.5 - 17.5", "14.3", true));
+    	fields.add(new TestFieldData("Parasites", "13.5 - 17.5", "14.6", true));
+    	fields.add(new TestFieldData("ESR", "13.5 - 17.5", "16.3", true));
+    	DetailRecord record = new DetailRecord(1, "Sukbhir Singh", "Male", "Blood", fields);
     	
     	return record;
     }
